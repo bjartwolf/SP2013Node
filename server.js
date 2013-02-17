2185382abb0f557c1ad32bbf3683a18b1ea3e830
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2013-02-17 18:15:27 +0100

    init

---
+++ server.js
@@ -0,0 +1,113 @@
+// how the f should this application be split up...
+// should probably hide a lot of the passport/connect/cookieparseing/socketsetup into one
+// login-module
+// and then have app-routes and socket.emits here
+var SharePointStrategy = require('./strategy.js'),
+    express = require('express'),
+    passport = require('passport'),
+    app = express(),
+    qs = require('querystring'),
+    http = require('http'),
+    keys = require('./keys.js'), // importing secret keys,
+    expressCookies = require('express/node_modules/cookie'),
+    connectUtils = require('express/node_modules/connect/lib/utils'),
+    MemoryStore = express.session.MemoryStore,
+    sessionStore = new MemoryStore;
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.set('views', __dirname + '/template');
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: keys.cookieSecret,
+                            store: sessionStore,
+                            key: 'express.sid'
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+});
+
+// serialize and deseriazlie is used by passport to store users in sessions based on user.id, sessions provided by express.session
+var users = [];
+passport.serializeUser(function(user, done) {
+  users[user.id] = user;
+  // this should just set userid in cookie. cookie is signed and secret
+  // not sure where this number is from, though, probably need to make
+  // a smarter id based on url and id or something... or a guid
+  // cookies should be https-only
+  // expires when browser session ends
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    var user = users[id];
+    if (user) {
+        done(null, users[id]);
+    } else {
+        done('User not found', null);
+    }
+});
+
+passport.use(new SharePointStrategy({
+    clientID: keys.clientID, 
+    clientSecret: keys.clientSecret
+  },
+  function(accessToken, refreshToken, profile, done) {
+        // Adds the tokens to the user
+        // It is later stored by the serialize/deserializeuser functions
+        profile.accessToken = accessToken;
+        profile.refreshToken = refreshToken;
+        return done(null, profile); 
+  }
+));
+
+//TODO: memorystore in session should be replaced with memcached or something
+//Mongo is free. 
+app.post('/authenticate/sharepoint',
+  passport.authenticate('sharepoint'),
+  function(req, res){
+    res.redirect('/'); 
+  });
+
+app.get('/', function (req, res) {
+  // should be restored by deserializer
+  // managed to get the users refreshtoken
+  if (req.user) {
+//    var refreshToken = req.user.refreshToken;
+    var token = req.user.id;
+    res.render('index.jade', {token: token, pageTitle: "authorized"});
+  } else {
+    res.render('index.jade', {token: 'NO TOKEn', pageTitle: "no allows.."});
+  }
+});
+
+var server = app.listen(process.env.port || 1337);
+var io = require('socket.io').listen(server);
+io.set('log level', 1);
+// of creates a namespace or room 
+// should create one pr. sharepoint-server?
+var SPio= io.of('/SPio');
+SPio.authorization(function(handshakeData, accept) {
+  // Look up the sid from the cookie (after parsing it)
+  // and connects it too  
+  if (!handshakeData.headers.cookie) return accept('socket.io: no found cookie.', false);
+ 
+  var signedCookies = expressCookies.parse(handshakeData.headers.cookie);
+  handshakeData.cookies = connectUtils.parseSignedCookies(signedCookies, keys.cookieSecret); 
+  sessionStore.get(handshakeData.cookies['express.sid'], function(err, session) {
+    if (err || !session) {
+        return accept('socket.io: no found session.', false);
+    }
+ 
+    // refactor this shit 
+    handshakeData.session = session;
+    handshakeData.session.user = users[session.passport.user];
+    if (handshakeData.session.user) {
+      return accept(null, true);
+    } else {
+      return accept('socket.io: no found session.user', false);
+    }
+  })
+});
+ 
+SPio.on('connection', function (client) {
+  var username = client.handshake.session.user.username;
+});
---
+++ strategy.js
@@ -0,0 +1,212 @@
+/**
+ * Module dependencies.
+ */
+var passport = require('passport')
+  , url = require('url')
+  , https = require('https')
+  , util = require('util')
+  , utils = require('./utils')
+  , jwt = require('jwt-simple')
+  , OAuth2 = require('oauth').OAuth2
+  , InternalOAuthError = require('./internaloautherror');
+
+var SP_AUTH_PREFIX = '/_layouts/15/OAuthAuthorize.aspx';
+
+/**
+ * `Strategy` constructor.
+ *
+ * @param {Object} options
+ * @param {Function} verify
+ * @api public
+ */
+function Strategy(options, verify) {
+  options = options || {}
+  passport.Strategy.call(this);
+  this.name = 'sharepoint';
+  this._verify = verify;
+  
+  if (!options.clientID) throw new Error('SharePointStrategy requires a clientID option');
+  if (!options.clientSecret) throw new Error('SharePointStrategy requires a clientSecret option');
+  
+  this._clientID = options.clientID;
+  this._clientSecret = options.clientSecret;
+  this._scope = options.scope;
+  this._scopeSeparator = options.scopeSeparator || ' ';
+  this._skipUserProfile = (options.skipUserProfile === undefined) ? false : options.skipUserProfile;
+}
+
+/**
+ * Inherit from `passport.Strategy`.
+ */
+util.inherits(Strategy, passport.Strategy);
+
+
+/**
+ * Authenticate request by delegating to the SharePoint OAuth 2.0 provider.
+ *
+ * @param {Object} req
+ * @api protected
+ */
+Strategy.prototype.authenticate = function(req, options) {
+  options = options || {};
+  var self = this;
+  
+  if (req.query && req.query.error) {
+    // TODO: Error information pertaining to OAuth 2.0 flows is encoded in the
+    //       query parameters, and should be propagated to the application.
+    return this.fail();
+  }
+  
+  spAppToken = undefined;
+  spHostUrl = undefined;
+      
+  // load token from request
+  if (req.body && req.body.SPAppToken)
+  	spAppToken = req.body.SPAppToken;
+  if(req.query && req.query.SPHostUrl)
+  	spHostUrl = req.query.SPHostUrl;
+  
+  //fallback to optional values
+  if (spAppToken == undefined || spHostUrl == undefined) {
+      spAppToken = options.SPAppToken;
+      spHostUrl = options.SPHostUrl;
+  }
+  // App token is sent to client from sharepoint, after sharepoint has talked to acs
+  if (spAppToken && spHostUrl) {
+    token = jwt.decode(spAppToken, '', true); // no need to eval here
+    //console.log('token: ' + token.refreshtoken);// Have refreshtoken here? Why? Ah, it is sent and encrypted in spapptoken
+    splitApptxSender = token.appctxsender.split("@");
+    sharepointServer = url.parse(spHostUrl)
+    resource = splitApptxSender[0]+"/"+sharepointServer.host+"@"+splitApptxSender[1];
+    spClientID = this._clientID+"@"+splitApptxSender[1];
+    appctx = JSON.parse(token.appctx);
+    tokenServiceUri = url.parse(appctx.SecurityTokenServiceUri);
+    authorizationURL = spHostUrl + SP_AUTH_PREFIX;
+    tokenURL = tokenServiceUri.protocol+'//'+tokenServiceUri.host+'/'+splitApptxSender[1]+tokenServiceUri.path;
+
+    this._oauth2 = new OAuth2(spClientID,  this._clientSecret, '', authorizationURL, tokenURL);
+    // Gets accesstoken, using the refreshtoken
+    this._oauth2.getOAuthAccessToken(
+        token.refreshtoken,
+        {grant_type: 'refresh_token', refresh_token: token.refreshtoken, resource: resource},
+        function (err, accessToken, refreshToken, params) {
+           if (err) { return self.error(new InternalOAuthError('failed to obtain access token', err)); }
+            if (!refreshToken)
+              refreshToken = token.refreshtoken;
+            // Has the accesstoken, loads the userprofile
+            self._loadUserProfile(accessToken, spHostUrl, function(err, profile) {
+                    if (err) { return self.error(err); };
+
+                    function verified(err, user, info) {
+                      if (err) { return self.error(err); }
+                      if (!user) { return self.fail(info); }
+                      self.success(user, info);
+                    }
+                    
+                    self._verify(accessToken, refreshToken, profile, verified);
+                });  
+        });
+  } else
+    return self.error(new InternalOAuthError('failed to obtain access token')); 
+}
+
+/**
+ * Retrieve user profile from SharePoint.
+ *
+ * @param {String} accessToken
+ * @param {Function} done
+ * @api protected
+ */
+Strategy.prototype.userProfile = function(accessToken, SPHostURL, done) {
+  //console.log('userprofile');
+  if (SPHostURL)
+    sharepointServer = url.parse(SPHostURL)
+  else
+    return done(null, {});
+    
+  var headers = {
+    'Accept': 'application/json;odata=verbose',
+    'Authorization' : 'Bearer ' + accessToken
+  };
+  var options = {
+    host : sharepointServer.hostname, 
+    port : sharepointServer.port,
+    path : sharepointServer.path + '/_api/web/currentuser', // Did not append slash correctly
+    method : 'GET',
+    headers : headers
+  };
+  var req = https.get(options, function(res) {
+    var userData = "";
+    res.on('data', function(data) {
+        userData += data;
+    });
+    
+    res.on('end', function() {
+      var json = JSON.parse(userData);
+      if (json.d) {
+        var profile = { provider: 'sharepoint' };
+        profile.id = json.d.Id;
+        profile.username = json.d.LoginName;
+        profile.displayName = json.d.Title;
+        profile.emails = [{ value: json.d.Email }];
+        profile.host = SPHostURL;
+        profile._raw = userData;
+        profile._json = json;
+        done(null, profile);
+      }
+      else
+        return done(null, {});
+    });
+  }).on('error', function(e) {
+    return done(null, {});
+  });
+}
+
+/**
+ * Return extra parameters to be included in the authorization request.
+ *
+ * @param {Object} options
+ * @return {Object}
+ * @api protected
+ */
+Strategy.prototype.authorizationParams = function(options) {
+  return {};
+}
+
+/**
+ * Load user profile, contingent upon options.
+ *
+ * @param {String} accessToken
+ * @param {Function} done
+ * @api private
+ */
+Strategy.prototype._loadUserProfile = function(accessToken, SPHostURL, done) {
+  var self = this;
+  //console.log('loading userprofile');
+  
+  function loadIt() {
+    return self.userProfile(accessToken, SPHostURL, done);
+  }
+  function skipIt() {
+    return done(null);
+  }
+  
+  if (typeof this._skipUserProfile == 'function' && this._skipUserProfile.length > 1) {
+    // async
+    this._skipUserProfile(accessToken, function(err, skip) {
+      if (err) { return done(err); }
+      if (!skip) { return loadIt(); }
+      return skipIt();
+    });
+  } else {
+    var skip = (typeof this._skipUserProfile == 'function') ? this._skipUserProfile() : this._skipUserProfile;
+    if (!skip) { return loadIt(); }
+    return skipIt();
+  }
+}
+
+/**
+ * Expose `Strategy`.
+ */ 
+module.exports = Strategy;
+
