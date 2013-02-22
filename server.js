3d7ad037ed5f93746b7bd8d64e45671d2580df72
bjartwolf <bjartnes@gmail.com>
2013-02-22 14:04:49 +0100

    refactoring midway

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ var SharePointStrategy = require('./strategy.js'),
     express = require('express'),
     passport = require('passport'),
     app = express(),
-    qs = require('querystring'),
     http = require('http'),
     https = require('https'),
-    expressCookies = require('express/node_modules/cookie'),
-    connectUtils = require('express/node_modules/connect/lib/utils'),
     MemoryStore = express.session.MemoryStore,
     sessionStore = new MemoryStore;
 
@@ -99,42 +96,8 @@ app.get('/', function (req, res) {
 });
 
 if (process.env.port) {
-    var server = https.createServer(options, app).listen(process.env.port);
-} else {
     var server = http.createServer(app).listen(process.env.port);
+} else {
+    var server = https.createServer(options, app).listen(1337);
 }
-var io = require('socket.io').listen(server);
-io.set('log level', 1);
-// of creates a namespace or room 
-// should create one pr. sharepoint-server?
-var SPio= io.of('/SPio');
-SPio.authorization(function(handshakeData, accept) {
-  // Look up the sid from the cookie (after parsing it)
-  // and connects it too  
-  if (!handshakeData.headers.cookie) return accept('socket.io: no found cookie.', false);
- 
-  var signedCookies = expressCookies.parse(handshakeData.headers.cookie);
-  handshakeData.cookies = connectUtils.parseSignedCookies(signedCookies, keys.cookieSecret); 
-  sessionStore.get(handshakeData.cookies['express.sid'], function(err, session) {
-    if (err || !session) {
-        return accept('socket.io: no found session.', false);
-    }
- 
-    // refactor this shit 
-    handshakeData.session = session;
-    handshakeData.session.user = users[session.passport.user];
-    if (handshakeData.session.user) {
-      return accept(null, true);
-    } else {
-      return accept('socket.io: no found session.user', false);
-    }
-  })
-});
- 
-SPio.on('connection', function (client) {
-  var username = client.handshake.session.user.username;
-  setInterval(function () { client.emit('sendTimer', 'do it');}, 1000);
-  client.on('timerSent', function (data) {
-        client.emit('timerPingback', data);
-  });
-});
+require('./sessionIO.js').startListen(server, keys, sessionStore, users);
