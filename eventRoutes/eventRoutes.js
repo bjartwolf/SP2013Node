f1d61b17c0a675d5c935767691810d8e279e06f9
bjartwolf <bjartnes@gmail.com>
2013-03-07 15:18:19 +0100

    added moveEvent broadcast

--- eventRoutes/eventRoutes.js
+++ eventRoutes/eventRoutes.js
@@ -4,9 +4,12 @@ module.exports = function (io) {
       console.log('connected socket io with user ' + client.handshake.session.user.username );
       setInterval(function () { 
           io.of('/SPio').emit('sendTimer', 'do it');
-        }, 30000);
+        }, 3000);
       client.on('timerSent', function (data) {
           client.emit('timerPingback', data);
+      });
+      client.on('moveEvent', function (moveEvent) {
+          client.broadcast.emit('moveEvent', moveEvent);
       });
     // Add more client.on here
     // The requests should be authenticated, and you can get profile info 
