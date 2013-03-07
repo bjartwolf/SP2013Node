abcae49e28ad3db546998b88fb2d6b8c42c063c2
bjartnes <bjartnes@gmail.com>
2013-03-07 16:31:08 +0100

    sample task object

--- static/viewModels/tasksViewModel.js
+++ static/viewModels/tasksViewModel.js
@@ -1,5 +1,14 @@
 (function (ns) {
 
+	var Task=function(){
+		var self = this;
+		this.id=0;
+		this.title=ko.observable('knockout js');
+		this.owner=ko.observable('Amr');
+		this.description=ko.observable('create a separate template of knockout js using a template block');
+		this.dueOn=ko.observable('07.03.2013');
+	};
+
     ns.TasksViewModel = function () {
 		var self = this;
 		
