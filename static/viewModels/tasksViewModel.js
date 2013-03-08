472496d8e191e2113f3658b9986efb38717ec7b2
jornare <jornare@hatlelid.net>
2013-03-08 09:34:01 +0100

    sockets pushing when an element changes

--- static/viewModels/tasksViewModel.js
+++ static/viewModels/tasksViewModel.js
@@ -44,7 +44,8 @@
 		    if (task.propagate === false) {
 		        return;
 		    }
-		    var msg = { id: task.id, property: value };
+		    var msg = { id: task.id };
+		    msg[property] = value;
 		    if (ns.socket) {
 		        socket.emit('moveEvent', msg);
 		    }
@@ -92,18 +93,19 @@
 		    for (var i = 0; i < self.tasks().length; i++) {
 		        if (tasks[i].id == msg.id) {
 		            delete msg.id;
-		            task[i].propagate = false;
+		            tasks[i].propagate = false;
 		            for (var p in msg) {
 		                tasks[i][p](msg[p]);
 		            }
-		            task[i].propagate = true;
+		            tasks[i].propagate = true;
 		            return;
 		        }
 		    }
 		});
 
 		ns.socket.on('newTask', function (task) {
-		    self.addTask(self.serverTask2AppTask(task));
+		    var task = self.serverTask2AppTask(task);
+		    self.addTask(task);
 		});
 		
     };
