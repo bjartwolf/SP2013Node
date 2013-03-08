4953267f9d79f08ab6a890e21cb57a3c5370653a
bjartnes <bjartnes@gmail.com>
2013-03-08 17:27:50 +0100

    smoother sliding between the dashboard and the create task form

--- static/tasks-ui.js
+++ static/tasks-ui.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('#createNewTask').click(function () {
         $('#createNewTask').hide("slide", { direction: "left" }, 1000);
         $('#taskboard').hide("slide", { direction: "left" }, 1000);
-        $('#createtask-form').show({ easing: 'easeOutExpo' });
+        $('#createtask-form').show("slide", { direction: "right", duration: 1000, easing: 'easeOutBack' });
     });
     $('.back-button').click(function () {
         $('#createtask-form').hide("slide", { direction: "right" }, 1000);
