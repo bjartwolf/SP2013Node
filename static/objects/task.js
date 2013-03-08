4ba4961a43163aec55cca5679c26c066366adde2
Askild Tønne <askildt@gmail.com>
2013-03-08 08:35:35 +0100

    Updated Task object to match SP

--- static/objects/task.js
+++ static/objects/task.js
@@ -1,13 +1,13 @@
 (function (exports) {
 	exports.Task = function(id, owner, title, description, priority, progress, dueOn) {
 		var self = this;
-		this.id = id;		
-		this.owner = owner;
-		this.title = title;
-		this.description = description;
-		this.priority = priority;
-		this.progress = progress;
-		this.dueOn = dueOn;
+		this.ID = id;		
+		this.Author = owner;
+		this.Title = title;
+		this.Body = description;
+		this.Priority = priority;
+		this.PercentComplete = progress;
+		this.DueDate = dueOn;
 		return self;
 	};
 })(typeof exports === 'undefined' ? this['Computas']={} : exports);
